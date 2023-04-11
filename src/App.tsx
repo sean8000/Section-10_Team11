fbf3eac34bb9ac03c7a7b3ee3ad1fae722a485e5
Sean Johnson <seanjohn@udel.edu>
2023-04-11 16:32:55 -0400

    Our placeholder website

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+//import {roleSelect} from "./roleSelect"
+function App(): JSX.Element {
+    return (
+        <div>
+            <h1> This is our website</h1>
+        </div>
+    );
+}
+export default App;
---
+++ src/roleSelect.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export function roleSelect(): JSX.Element {
+    return (
+        <div>
+            <h1> This is our website</h1>
+        </div>
+    );
+}
