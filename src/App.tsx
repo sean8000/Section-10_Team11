f692f34c9f8851feb849e7696f43520d558a579a
alexmarshall11 <alexmar@udel.edu>
2023-04-17 12:30:42 -0400

    Added Alexander Marshall

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
     return (
         <div>
             <h1> This is our website</h1>
+            <span>Alexander Marshall</span>
             <Test></Test>
             <Container>
                 <Row>
