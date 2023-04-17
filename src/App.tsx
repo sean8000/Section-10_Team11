1a30f27ff66697f3022d4a295afd64b659a039f3
Dean Turner <turnerde@udel.edu>
2023-04-17 10:40:45 -0400

    formated the playera little better still need to work on DnD

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Test from "./components/Test";
 //import {roleSelect} from "./roleSelect"
 import { Col, Container, Row } from "react-bootstrap";
 import { RoleSelect } from "./roleSelect";
+import playerBox from "./components/playerBox";
+import playerBucket from "./components/playerBucket";
 
 function App(): JSX.Element {
     return (
@@ -13,10 +15,6 @@ function App(): JSX.Element {
                 <Row>
                     <Col>
                         <h2> List 1: Central List</h2>
-                        <img
-                            src="https://www.gettyimages.com/photos/american-football-player"
-                            alt="player"
-                        />
                     </Col>
                     <Col>
                         <h2> List 2: Admin List</h2>
