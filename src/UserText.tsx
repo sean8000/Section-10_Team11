8c1decdddad3df726958604683dcaf1687943422
Sean Johnson <seanjohn@udel.edu>
2023-04-18 17:11:47 -0400

    Added adding user functionality, moving up use state to save user's list state

---
+++ src/UserText.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+interface Users {
+    userText: string;
+    setUserText: (newString: string) => void;
+}
+export function UserText({ userText, setUserText }: Users): JSX.Element {
+    // This is the Control
+    function updateText(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserText(event.target.value);
+    }
+
+    // This is the View
+    return (
+        <div>
+            <Form.Group controlId="UserTextBox">
+                <Form.Label>User Name:</Form.Label>
+                <Form.Control value={userText} onChange={updateText} />
+            </Form.Group>
+            <div>User Name: {userText} </div>
+        </div>
+    );
+}
